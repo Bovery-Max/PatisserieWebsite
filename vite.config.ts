9153b177f483ae20217174e852a9912f5daff1fb
Bovery-Max <eigsanall@hotmail.com>
2026-06-12 13:56:46 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   
+base: '/PatisserieWebsite/',
   server: {
     port: 5173,
     strictPort: true  // port doluysa hata ver, başka porta geçme
   }
 })
+
 
